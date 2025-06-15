eb05efadf8449e7252ac376b1d30ad69ff8c64ec
Aleksandr Isupov <115366324+CAN4red@users.noreply.github.com>
2025-06-15 17:28:29 +0300

    feat: set up dev connection (#9)
    * chore: update configuration files
    
    * chore: set up retrofit and hilt gradle dependencies
    
    * feat: add retrofit di module
    
    * feat: add example dto and api
    
    * feat: set up dev connection

--- app/src/main/java/com/example/evops/app/MainActivity.kt
+++ app/src/main/java/com/example/evops/app/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.evops.app
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -10,12 +11,22 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.lifecycleScope
 import com.example.evops.core.presentation.EvOpsTheme
+import com.example.evops.screens.eventlist.data.dto.ExampleDto
+import com.example.evops.screens.eventlist.domain.repository.ExampleRepository
 import com.example.evops.screens.eventlist.presentation.components.PreviewData
 import com.example.evops.screens.eventlist.presentation.components.eventcard.EventCard
 import com.example.evops.screens.eventlist.presentation.components.searchfield.SearchField
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @Inject
+    lateinit var repository: ExampleRepository
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -36,6 +47,19 @@ class MainActivity : ComponentActivity() {
                         )
                     }
                 }
+            }
+        }
+        lifecycleScope.launch {
+            try {
+                val result = repository.createEvent(
+                    ExampleDto(
+                        description = "max lox",
+                        name = "lol"
+                    )
+                )
+                Log.d("DEBUG API", "name: ${result.name}, description: ${result.description}")
+            } catch (e: Exception) {
+                Log.e("DEBUG API", "Create event failed", e)
             }
         }
     }

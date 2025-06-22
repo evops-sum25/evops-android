a0f21c63dcd62ea0bbc5195f6ed08557434029f8
can4red <CAN4red@yandex.ru>
2025-06-22 12:25:43 +0300

    feat: add di network module

---
+++ app/src/main/java/com/example/evops/screens/evendetails/di/EventDetailsNetworkModule.kt
@@ -0,0 +1,28 @@
+package com.example.evops.screens.evendetails.di
+
+import com.example.evops.screens.evendetails.data.EventDetailsNetworkRepositoryImpl
+import com.example.evops.screens.evendetails.data.api.EventDetailsApi
+import com.example.evops.screens.evendetails.domain.repository.EventDetailsNetworkRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object EventDetailsNetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideEventDetailsApi(retrofit: Retrofit): EventDetailsApi {
+        return retrofit.create(EventDetailsApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideEventDetailsRepository(api: EventDetailsApi): EventDetailsNetworkRepository {
+        return EventDetailsNetworkRepositoryImpl(api)
+    }
+}

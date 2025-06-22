089de226fdd45e01a604b826ac76ec32c151bfd9
can4red <CAN4red@yandex.ru>
2025-06-22 12:16:24 +0300

    feat: add api interface and repos

--- app/src/main/java/com/example/evops/screens/evendetails/domain/model/EventDetailsPublisher.kt
+++ app/src/main/java/com/example/evops/screens/evendetails/domain/model/EventDetailsPublisher.kt
@@ -1,6 +1,6 @@
 package com.example.evops.screens.evendetails.domain.model
 
-data class EventDetailsPublisherData(
+data class EventDetailsPublisher(
     val id: String = "", // TODO("change to not have a default value")
     val name: String,
     val avatarPreviewUrl: String,

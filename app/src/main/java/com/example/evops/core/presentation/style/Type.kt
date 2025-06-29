8d1ad0b886bf90d444d9bf432a40150e942b34b5
can4red <CAN4red@yandex.ru>
2025-06-29 17:55:40 +0300

    feat: add open sans font and create simple typography
    # Conflicts:
    #	app/src/main/java/com/example/evops/core/presentation/style/Type.kt

--- app/src/main/java/com/example/evops/core/presentation/style/Type.kt
+++ app/src/main/java/com/example/evops/core/presentation/style/Type.kt
@@ -2,62 +2,35 @@ package com.example.evops.core.presentation.style
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
-import com.example.evops.R
 
-private val OpenSans =
-    FontFamily(
-        Font(R.font.open_sans_light, FontWeight.Light),
-        Font(R.font.open_sans, FontWeight.Normal),
-        Font(R.font.open_sans_medium, FontWeight.Medium),
-        Font(R.font.open_sans_semibold, FontWeight.SemiBold),
-        Font(R.font.open_sans_bold, FontWeight.Bold),
-        Font(R.font.open_sans_extrabold, FontWeight.ExtraBold),
-    )
-
+// Set of Material typography styles to start with
 val Typography =
     Typography(
-        displayMedium =
-            TextStyle(
-                fontFamily = OpenSans,
-                fontWeight = FontWeight.Medium,
-                fontSize = 20.sp,
-                lineHeight = 24.sp,
-                letterSpacing = 0.15.sp,
-            ),
-        titleMedium =
-            TextStyle(
-                fontFamily = OpenSans,
-                fontWeight = FontWeight.Bold,
-                fontSize = 24.sp,
-                lineHeight = 28.sp,
-                letterSpacing = 0.15.sp,
-            ),
         bodyLarge =
             TextStyle(
-                fontFamily = OpenSans,
-                fontWeight = FontWeight.Medium,
+                fontFamily = FontFamily.Default,
+                fontWeight = FontWeight.Normal,
                 fontSize = 16.sp,
-                lineHeight = 20.sp,
-                letterSpacing = 0.25.sp,
-            ),
-        bodyMedium =
-            TextStyle(
-                fontFamily = OpenSans,
-                fontWeight = FontWeight.Normal,
-                fontSize = 14.sp,
-                lineHeight = 18.sp,
-                letterSpacing = 0.25.sp,
-            ),
-        labelMedium =
-            TextStyle(
-                fontFamily = OpenSans,
-                fontWeight = FontWeight.Normal,
-                fontSize = 12.sp,
-                lineHeight = 16.sp,
+                lineHeight = 24.sp,
                 letterSpacing = 0.5.sp,
             ),
+    /* Other default text styles to override
+    titleLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 11.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
+    )
+     */
     )

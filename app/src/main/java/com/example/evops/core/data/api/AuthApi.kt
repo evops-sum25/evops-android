ba50a2bae5439dbb67ea8dc7c3399d7f196e509c
can4red <CAN4red@yandex.ru>
2025-07-21 12:36:54 +0300

    feat: add auth api

--- app/src/main/java/com/example/evops/core/data/api/AuthApi.kt
+++ app/src/main/java/com/example/evops/core/data/api/AuthApi.kt
@@ -1,3 +1,18 @@
 package com.example.evops.core.data.api
 
-interface AuthApi {}
+import com.example.evops.core.data.model.auth.CredentialsWrapperDto
+import com.example.evops.core.data.model.auth.RefreshTokenWrapperDto
+import com.example.evops.core.data.model.auth.SignupFormWrapperDto
+import com.example.evops.core.data.model.auth.TokensWrapperDto
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface AuthApi {
+    @POST("/v1/auth/login")
+    suspend fun login(@Body credentials: CredentialsWrapperDto): TokensWrapperDto
+
+    @POST("/v1/auth/refresh")
+    suspend fun refresh(@Body refreshToken: RefreshTokenWrapperDto): TokensWrapperDto
+
+    @POST("/v1/auth/signup") suspend fun signup(@Body form: SignupFormWrapperDto): TokensWrapperDto
+}
---
+++ app/src/main/java/com/example/evops/core/data/model/auth/SignupFormWrapperDto.kt
@@ -0,0 +1,5 @@
+package com.example.evops.core.data.model.auth
+
+import kotlinx.serialization.Serializable
+
+@Serializable data class SignupFormWrapperDto(val form: SignupFormDto)

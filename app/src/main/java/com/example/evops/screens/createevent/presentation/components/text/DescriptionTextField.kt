af5b861c108806679a13f0471342f8987c7db2a4
can4red <CAN4red@yandex.ru>
2025-07-22 01:20:17 +0300

    feat: add description validation

--- app/src/main/java/com/example/evops/screens/createevent/presentation/components/text/DescriptionTextField.kt
+++ app/src/main/java/com/example/evops/screens/createevent/presentation/components/text/DescriptionTextField.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -20,6 +21,11 @@ import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.TextFieldValue
 import com.example.evops.R
 import com.example.evops.screens.createevent.presentation.CreateEventEvent
+import uniffi.evops.ValidateEventDescriptionResult
+import uniffi.evops.getEventDescriptionLenCharMin
+import uniffi.evops.getEventTitleLenCharMax
+import uniffi.evops.validateEventDescription
+import kotlin.math.max
 
 @Composable
 fun DescriptionTextField(
@@ -28,6 +34,12 @@ fun DescriptionTextField(
     modifier: Modifier = Modifier,
 ) {
     var textFieldValue by remember { mutableStateOf(TextFieldValue(description)) }
+    val descriptionValidationResult by remember {
+        derivedStateOf {
+            if (textFieldValue.text.isEmpty()) ValidateEventDescriptionResult.OK
+            else validateEventDescription(textFieldValue.text)
+        }
+    }
 
     LaunchedEffect(description) {
         if (description != textFieldValue.text) {
@@ -45,7 +57,10 @@ fun DescriptionTextField(
         label = { DescriptionLabel() },
         suffix = { SuggestTagsButton(onEvent = onEvent) },
         minLines = 3,
+        maxLines = 3,
         keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+        isError = descriptionValidationResult != ValidateEventDescriptionResult.OK,
+        supportingText = { ValidationMessage(descriptionValidationResult) },
         modifier = modifier.fillMaxWidth(),
     )
 }
@@ -58,6 +73,19 @@ private fun SuggestTagsButton(onEvent: (CreateEventEvent) -> Unit, modifier: Mod
     ) {
         Icon(painter = painterResource(R.drawable.wand_stars), contentDescription = null)
     }
+}
+
+@Composable
+private fun ValidationMessage(result: ValidateEventDescriptionResult) {
+    val message =
+        when (result) {
+            ValidateEventDescriptionResult.OK -> null
+            ValidateEventDescriptionResult.LEN_CHAR_MIN_VIOLATED ->
+                stringResource(R.string.description_min_len_error, getEventDescriptionLenCharMin())
+            ValidateEventDescriptionResult.LEN_CHAR_MAX_VIOLATED ->
+                stringResource(R.string.description_max_len_error, getEventTitleLenCharMax())
+        }
+    message?.let { Text(text = message) }
 }
 
 @Composable

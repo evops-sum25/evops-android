3a568febabe2734c227ea4665fc22599cde6993e
can4red <CAN4red@yandex.ru>
2025-07-22 01:26:09 +0300

    feat: add title validation

--- app/src/main/java/com/example/evops/screens/createevent/presentation/components/text/TitleTextField.kt
+++ app/src/main/java/com/example/evops/screens/createevent/presentation/components/text/TitleTextField.kt
@@ -6,6 +6,7 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -17,6 +18,10 @@ import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.TextFieldValue
 import com.example.evops.R
 import com.example.evops.screens.createevent.presentation.CreateEventEvent
+import uniffi.evops.ValidateEventTitleResult
+import uniffi.evops.getEventTitleLenCharMax
+import uniffi.evops.getEventTitleLenCharMin
+import uniffi.evops.validateEventTitle
 
 @Composable
 fun TitleTextField(
@@ -25,6 +30,12 @@ fun TitleTextField(
     modifier: Modifier = Modifier,
 ) {
     var textFieldValue by remember { mutableStateOf(TextFieldValue(title)) }
+    val titleValidationResult by remember {
+        derivedStateOf {
+            if (textFieldValue.text.isEmpty()) ValidateEventTitleResult.OK
+            else validateEventTitle(textFieldValue.text)
+        }
+    }
 
     LaunchedEffect(title) {
         if (title != textFieldValue.text) {
@@ -41,8 +52,23 @@ fun TitleTextField(
         label = { TitleLabel() },
         singleLine = true,
         keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+        isError = titleValidationResult != ValidateEventTitleResult.OK,
+        supportingText = { ValidationMessage(titleValidationResult) },
         modifier = modifier.fillMaxWidth(),
     )
+}
+
+@Composable
+private fun ValidationMessage(result: ValidateEventTitleResult) {
+    val message =
+        when (result) {
+            ValidateEventTitleResult.OK -> null
+            ValidateEventTitleResult.LEN_CHAR_MIN_VIOLATED ->
+                stringResource(R.string.title_min_len_error, getEventTitleLenCharMin())
+            ValidateEventTitleResult.LEN_CHAR_MAX_VIOLATED ->
+                stringResource(R.string.title_max_len_error, getEventTitleLenCharMax())
+        }
+    message?.let { Text(text = message) }
 }
 
 @Composable

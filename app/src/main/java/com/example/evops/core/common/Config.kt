2bd1420a7e6ed848e0d9609576fc513edb150602
can4red <CAN4red@yandex.ru>
2025-07-22 16:45:05 +0300

    chore: update base url to prod

--- app/src/main/java/com/example/evops/core/common/Config.kt
+++ app/src/main/java/com/example/evops/core/common/Config.kt
@@ -3,7 +3,7 @@ package com.example.evops.core.common
 object Config {
     private const val DEV_URL = "http://10.0.2.2:8080"
     private const val PROD_URL = "http://31.59.170.53:8080"
-    const val BASE_URL = DEV_URL
+    const val BASE_URL = PROD_URL
     const val EVENT_PAGE_LIMIT = 20
 
     fun constructImageUrl(imageId: String): String {
